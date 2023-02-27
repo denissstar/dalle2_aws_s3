12428a12e204741107afa8948e251cd3bb374d18
Deniss Starevics <deniss.starevics@brambles.com>
2023-02-27 15:47:12 +0000

    Add upload to s3 method, cleanup and added new examples

--- README.md
+++ README.md
@@ -5,29 +5,33 @@
 </p>
 
 This is a simple working example on how to integrate the DALLE2 API into your own Python project and create amazing images directly from the command line.
+Now with AWS S3 integration!
 
 ## How it works
 Reads a string from the command line in natural language and creates an input prompt which is then fed to OpenAIs DALLE2. The response contains urls to the generated images. The images are downloaded and stored in an output folder.
 To generate your own images you need to get access to the DALLE2 API (https://openai.com/dall-e-2/).
 
 ## Installation
+For best practices install requirements in activated virtualenv
 ```bash
-git clone https://github.com/alxschwrz/dalle2_python.git
-cd dalle2_python
+git clone git@github.com:denissstar/dalle2_aws_s3.git
+cd dalle2_aws_s3
 pip3 install -r requirements.txt
 ```
+
+Update `self._s3_bucket` variable with your S3 bucket name
 
 ## Run example
 ```
 python3 dalle2_python.py
-What image should dalle create: if running late for class was an animal, digital art
-Generated image stored in: ./output/if running late for class was an animal, digital art_0.png
-Generated image stored in: ./output/if running late for class was an animal, digital art_1.png
+What image should dalle create: Cats do cloud engineering, by Banksy (Street Art)
+Generated images stored in: ./output/*.png
+Generated images uploaded to S3 bucket with same key name as the filename
 ```
 Now check the output folder:
 <p float="left">
-<img src="/output/if running late for class was an animal, digital art_0.png" alt="example_of_prompt_in_terminal" width="200"/>
-<img src="/output/if running late for class was an animal, digital art_1.png" alt="example_of_prompt_in_terminal" width="200"/>
+<img src="/output/Cats do cloud engineering, by Banksy (Street Art)_0.png" alt="example_of_prompt_in_terminal" width="200"/>
+<img src="/output/Cats do cloud engineering, by Salvador Dali_1.png" alt="example_of_prompt_in_terminal" width="200"/>
 </p>
 
 Enjoy!
--- dalle2_python.py
+++ dalle2_python.py
@@ -1,88 +1,117 @@
 import os
 import configparser
+import logging
+import requests
 import sys
 import webbrowser
 import urllib.request
+
+import boto3
+from botocore.exceptions import ClientError
 import openai
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
+
+# based on https://github.com/alxschwrz/dalle2_python/blob/main/dalle2_python.py
 class Dalle:
-        def __init__(self, img_sz="512", n_images=2):
-                self._api_keys_location = "./config"
-                self._generated_image_location = "./output"
-                self._stream = True
-                self._img_sz = img_sz
-                self._n_images = n_images
-                self._image_urls = []
-                self._input_prompt = None
-                self._response = None
-                self.initialize_openai_api()
+    def __init__(self, img_sz="512", n_images=2):
+        self._api_keys_location = "./config"
+        self._generated_image_location = "./output"
+        self._s3_bucket = "dalle2-image-bucket"
+        self._stream = True
+        self._img_sz = img_sz
+        self._n_images = n_images
+        self._image_urls = []
+        self._input_prompt = None
+        self._response = None
+        self.initialize_openai_api()
 
-        def create_template_ini_file(self):
-                """
-                If the ini file does not exist create it and add the organization_id and
-                secret_key
-                """
-                if not os.path.isfile(self._api_keys_location):
-                        with open(self._api_keys_location, 'w') as f:
-                                f.write('[openai]\n')
-                                f.write('organization_id=\n')
-                                f.write('secret_key=\n')
+    def create_template_ini_file(self):
+        """
+        If the ini file does not exist create it and add the organization_id and
+        secret_key
+        """
+        if not os.path.isfile(self._api_keys_location):
+            with open(self._api_keys_location, "w") as f:
+                f.write("[openai]\n")
+                f.write("organization_id=\n")
+                f.write("secret_key=\n")
 
-                        print('OpenAI API config file created at {}'.format(self._api_keys_location))
-                        print('Please edit it and add your organization ID and secret key')
-                        print('If you do not yet have an organization ID and secret key, you\n'
-                                'need to register for OpenAI Codex: \n'
-                                'https://openai.com/blog/openai-codex/')
-                        sys.exit(1)
+            print(f"""OpenAI API config file created at {self._api_keys_location}
+                Please edit it and add your organization ID and secret key
+                If you do not yet have an organization ID and secret key, you\n
+                need to register for OpenAI Codex: \n
+                https://openai.com/blog/openai-codex/""")
+            sys.exit(1)
 
 
-        def initialize_openai_api(self):
-                """
-                Initialize the OpenAI API
-                """
-                # Check if file at API_KEYS_LOCATION exists
-                self.create_template_ini_file()
-                config = configparser.ConfigParser()
-                config.read(self._api_keys_location)
+    def initialize_openai_api(self):
+        """
+        Initialize the OpenAI API
+        """
+        # Check if file at API_KEYS_LOCATION exists
+        self.create_template_ini_file()
+        config = configparser.ConfigParser()
+        config.read(self._api_keys_location)
 
-                openai.organization_id = config['openai']['organization_id'].strip('"').strip("'")
-                openai.api_key = config['openai']['secret_key'].strip('"').strip("'")
-                del config
+        openai.organization_id = config["openai"]["organization_id"].strip('"').strip("'")
+        openai.api_key = config["openai"]["secret_key"].strip('"').strip("'")
+        del config
 
-        def read_from_command_line(self):
-                self._input_prompt = input("What image should dalle create: ")
+    def read_from_command_line(self):
+        self._input_prompt = input("What image should dalle create: ")
 
-        def generate_image_from_prompt(self):
-                self._response = openai.Image.create(
-                        prompt=self._input_prompt,
-                        n=self._n_images,
-                        size=f"{self._img_sz}x{self._img_sz}",
-                )
-                
-        def get_urls_from_response(self):
-                for i in range(self._n_images):
-                        self._image_urls.append(self._response['data'][i]['url'])
-
-        def open_urls_in_browser(self, image_urls=None):
-                if image_urls is None:
-                        image_urls = self._image_urls
-                for url in image_urls:
-                        webbrowser.open(url)
-
-        def save_urls_as_image(self):
-                if not os.path.isdir(self._generated_image_location):
-                        os.mkdir(self._generated_image_location)
-                for idx, image_url in enumerate(self._image_urls):
-                        file_name = f"{self._generated_image_location}/{self._input_prompt}_{idx}.png"
-                        urllib.request.urlretrieve(image_url, file_name)
-                        print(f"Generated image stored in: {file_name}")
+    def generate_image_from_prompt(self):
+        self._response = openai.Image.create(
+            prompt=self._input_prompt,
+            n=self._n_images,
+            size=f"{self._img_sz}x{self._img_sz}",
+        )
         
-        def generate_and_save_images(self):
-                self.read_from_command_line()
-                self.generate_image_from_prompt()
-                self.get_urls_from_response()
-                self.save_urls_as_image()
+    def get_urls_from_response(self):
+        for i in range(self._n_images):
+            self._image_urls.append(self._response["data"][i]["url"])
+
+    def open_urls_in_browser(self, image_urls=None):
+        if image_urls is None:
+            image_urls = self._image_urls
+        for url in image_urls:
+            webbrowser.open(url)
+
+    def save_urls_as_image(self):
+        if not os.path.isdir(self._generated_image_location):
+            os.mkdir(self._generated_image_location)
+        for idx, image_url in enumerate(self._image_urls):
+            file_name = os.path.join(self._generated_image_location, f"{self._input_prompt}_{idx}.png")
+            urllib.request.urlretrieve(image_url, file_name)
+            logger.info(f"Generated image stored in: {file_name}")
+    
+    def generate_and_save_images(self):
+        self.read_from_command_line()
+        self.generate_image_from_prompt()
+        self.get_urls_from_response()
+        self.save_urls_as_image()
+        self.save_urls_as_s3_objects()
+
+    def save_urls_as_s3_objects(self):
+        """Upload a file to an S3 bucket
+
+        :param file_name: File to upload
+        :param bucket: Bucket to upload to
+        :param object_name: S3 object name. If not specified then file_name is used
+        :return: True if file was uploaded, else False
+        """
+        for idx, image_url in enumerate(self._image_urls):
+            r = requests.get(image_url, stream=True)
+            s3_client = boto3.client("s3")
+            s3_object_name = f"{self._input_prompt}_{idx}.png"
+            try:
+                s3_client.upload_fileobj(r.raw, self._s3_bucket, s3_object_name)
+            except ClientError as e:
+                logger.error(e)
+            logger.info(f"Generated image stored in: {self._s3_bucket} as {s3_object_name}")
 
 commandLineDalle = Dalle()
 commandLineDalle.generate_and_save_images()
